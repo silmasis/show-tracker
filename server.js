506cc6610fe0595d5599d469e0d9217660dc7a63
Silavong <silmasis@gmail.com>
2020-09-21 18:00:49 -0400

    cors issue

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, {
