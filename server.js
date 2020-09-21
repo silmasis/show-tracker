6b9f9cf5b9a4470aa2ab595743734df868ef6eb1
Silavong <silmasis@gmail.com>
2020-09-21 19:08:18 -0400

    Heroku Test

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 });
 
 mongoose.connection.on('connected', () => {
-    console.log('Mongoose is connected!!!!')
+    console.log('Mongoose is connected!!!')
 });
 
 app.use(bodyParser.json());
