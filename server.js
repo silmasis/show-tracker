9bbf38d632acde6362e6e8a4eb8f0694166492b9
Silavong <silmasis@gmail.com>
2020-09-21 17:52:26 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 
-const routes = require('./routes')
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -28,7 +26,6 @@ app.use('/users', usersRouter);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/', routes)
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static( '../client/build' ));
