2efba3a3ce859ec25e71f96b3e4f195197f50d34
Silavong <silmasis@gmail.com>
2020-09-21 17:27:24 -0400

    heroku deploy

--- server/config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://silmasis:testing123@cluster0.vf64g.gcp.mongodb.net/animeList?retryWrites=true&w=majority'
-}
--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const cors = require('cors');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
-const config = require("./config/key");
+const routes = require('./routes')
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.config, {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -19,22 +20,25 @@ mongoose.connection.on('connected', () => {
     console.log('Mongoose is connected!!!!')
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello There!'
-    });
-});
-
 const showRouter = require('./routes/shows');
 const usersRouter = require('./routes/users');
 
 app.use('/shows', showRouter);
 app.use('/users', usersRouter);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', routes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static( '../client/build' ));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${PORT}`);
 });
