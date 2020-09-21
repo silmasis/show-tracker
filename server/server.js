1a08b8853669bfa630db870ab3666e5791948b1d
Silavong <silmasis@gmail.com>
2020-09-21 16:55:24 -0400

    server folder

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
-
 const config = require("./config/key");
 
 const app = express();
@@ -12,9 +10,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB database connection established successfully'))
-    .catch(err => console.log(err));
+mongoose.connect(process.env.config, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!!!!')
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello There!'
+    });
+});
 
 const showRouter = require('./routes/shows');
 const usersRouter = require('./routes/users');
@@ -22,13 +34,6 @@ const usersRouter = require('./routes/users');
 app.use('/shows', showRouter);
 app.use('/users', usersRouter);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-
-    app.get("*", (req, res) =>{
-        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
-    });
-}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
