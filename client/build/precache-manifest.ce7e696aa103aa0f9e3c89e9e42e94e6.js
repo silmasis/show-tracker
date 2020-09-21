e2bfd31792678ee8d8e9b7aa106e2a945b5d7c78
Silavong <silmasis@gmail.com>
2020-09-21 14:19:09 -0400

    added build

---
+++ client/build/precache-manifest.ce7e696aa103aa0f9e3c89e9e42e94e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f07d58cfd8ff79301946998706a5d28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb8f728ffb47e01eca0e",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bb8f728ffb47e01eca0e",
+    "url": "/static/js/2.8be87d48.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8be87d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d1ecb63556b7a6e82b",
+    "url": "/static/js/main.f979c523.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
