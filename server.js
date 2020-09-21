b0d73cfd08d3ffba3cf58ea0fa5346833bce4ba8
Silavong <silmasis@gmail.com>
2020-09-21 14:21:00 -0400

    linked build in server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/shows', showRouter);
 app.use('/users', usersRouter);
 
 if (process.env.NODE_ENV === 'production') {
-    
+    app.use(express.static('client/build'));
 }
 
 app.listen(port, () => {
