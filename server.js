dc7acb0f660af2980ce24450bab31b69eb06cbfa
Silavong <silmasis@gmail.com>
2020-09-24 13:45:11 -0400

    updated server

--- server.js
+++ server.js
@@ -4,30 +4,31 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
 
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully')
+});
+
 
 const showRouter = require('./routes/shows');
 const usersRouter = require('./routes/users');
 
 app.use('/shows', showRouter);
 app.use('/users', usersRouter);
-
-app.use(cors());
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-mongoose.connection.on('connected', () => {
-    console.log('Mongoose is connected!!!')
-});
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static( 'client/build' ));
